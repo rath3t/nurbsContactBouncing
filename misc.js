93b8f97227c2fc2b74aad93cc7325a2bc0790eb4
rath3t <rathet@gmail.com>
2015-03-18 17:15:40 +0100

    gravity

--- misc.js
+++ misc.js
@@ -3,6 +3,6 @@ var checkandYield = function(param,name){
 	if(param || param === 0){
 		return param;
 	}else{
-		throw new Error('Parameter not defined:'+ name);
+		throw new Error('Parameter not defined: '+ name);
 	}
 }
--- physics.js
+++ physics.js
@@ -1,10 +1,4 @@
-//General functions
-
-
-
 ////////predefined 
-var gravity = -9.81;
-
 var knotvec_circ = [0,0,0,0.25,0.25,0.5,0.5,0.75,0.75,1,1,1]; //knotvec_circ and ptsweights_circ hold true for ellipses
 var ptsweights_circ = [1, 0.70710678118, 1, 0.70710678118, 1, 0.70710678118,1,0.70710678118,1];
 ////////end predefined
@@ -13,10 +7,10 @@ var ptsweights_circ = [1, 0.70710678118, 1, 0.70710678118, 1, 0.70710678118,1,0.
 
 
 
-
-var gravity =function(){ //gravity only chances values in global y-direction
-	//process gravity on all phys_objs
-	//update positons/speed/acc
+var gravity =function(phys_obj_array){
+	for (var i = 0; i < phys_obj_array.length; i++) {
+		phys_obj_array[i].acc.x2 =phys_obj_array[i].acc.x2 -9.81;
+	};
 }
 
 var collisionHandler = function(){
@@ -30,12 +24,12 @@ var collisionHandler = function(){
 	// normal calculation
 }
 
-var Phys_obj = function(pos,mass,speed,acc){
-	this.pos = new Vector2d(0,0,0);  //pos.x1 =x position, pos.x2 =y position, pos.x3 =orientation_angle,
+var Phys_obj = function(pos,mass,speed,acc,center){
+	this.pos = pos;  //pos.x1 =x position, pos.x2 =y position, pos.x3 =orientation_angle,
 	this.mass = mass;
 	this.speed = speed; //speed.x1= speed x-direction... speed.x3 = rotation x3 axis
 	this.acc = acc; // acc = [0,0,0]
-	this.center = 0;
+	this.center = center;
 	this.shapeData = {
 		degree: 0,
 		knotvec: 0,
@@ -49,7 +43,7 @@ var Phys_obj = function(pos,mass,speed,acc){
 	this.generateShape= function() { //generate shape from verbData
 		this.shape = new verb.geom.NurbsCurve(this.nurbsData);
 	}
-	this.move = function() { //update pos, speed
+	this.move = function() { //update pos, speed //x3 values are rotationvalues
 		this.pos = this.pos.add((this.speed.multi_scalar(tick)).add(this.acc.multi_scalar(Math.pow(tick,2))));//this.shape = new verb.geom.NurbsCurve(this.nurbsData);
 		this.speed = this.speed.add(this.acc.multi_scalar(tick));
 	}
@@ -57,14 +51,10 @@ var Phys_obj = function(pos,mass,speed,acc){
 
 
 var Circle = function(radius,pos,mass){
-	this.radius = radius;
-	this.prototype.pos 	= pos;
-	if(this.mass === undefined){
-		throw new Error('Physical objects need mass!')
-	}else{
-		this.mass   = mass;
-	}
+	this.radius = checkandYield(radius,"radius");
+	this.__proto__.pos 	= pos;
 	this.center = new Vector2d(this.pos.x1+this.radius,this.pos.x2+this.radius,0);
+	this.mass   = checkandYield(mass,"mass");
 	this.shapeData.degree = 2;
 	this.shapeData.knotvec = knotvec_circ;
 	this.shapeData.weights = ptsweights_circ;
@@ -91,35 +81,32 @@ Circle.prototype = new Phys_obj;
 Circle.prototype.constructor = Circle;
 
 var Ellipse = function(x1axis,x2axis,pos,mass){
-	this.x1axis = checkandYield(x1axis,"x1axis";
-	this.x2axis = x2axis;
-	this.pos 	= pos;
-	if(this.mass === undefined){
-		throw new Error('Physical objects need mass!')
-	}else{
-		this.mass   = mass;
-	}
+	this.x1axis = checkandYield(x1axis,"x1axis");
+	this.x2axis = checkandYield(x2axis,"x2axis");
+	this.pos 	= checkandYield(pos,"pos");
+	this.mass   = checkandYield(mass,"mass");
 	this.center = new Vector2d(this.pos.x1+this.x1axis,this.pos.x2+this.x2axis,0);
-	this.massIntertia
+	this.massIntertia = 
 	this.shapeData.degree = 2;
 	this.shapeData.knotvec = knotvec_circ;
 	this.shapeData.weights = ptsweights_circ;
 	this.generate_circle_cpts = function(){
-		var p1b= [this.pos.x1,                this.pos.x2+this.x2axis,     0];
-        var p2b= [this.pos.x1,                this.pos.x2,                 0]; 
-        var p3b= [this.pos.x1+this.x1axis,    this.pos.x2,                 0]; 
-        var p4b= [this.pos.x1+2*this.x1axis,  this.pos.x2,                 0]; 
-        var p5b= [this.pos.x1+2*this.x1axis,  this.pos.x2+this.x2axis,     0]; 
-        var p6b= [this.pos.x1+2*this.x1axis,  this.pos.x2+2*this.x2axis,   0]; 
-       	var p7b= [this.pos.x1+this.x1axis,    this.pos.x2+2*this.x2axis,   0];
-        var p8b= [this.pos.x1,                this.pos.x2+2*this.x2axis,   0];
+		var p1b= new Vector2d(this.pos.x1,                this.pos.x2+this.x2axis,     0);
+        var p2b= new Vector2d(this.pos.x1,                this.pos.x2,                 0); 
+        var p3b= new Vector2d(this.pos.x1+this.x1axis,    this.pos.x2,                 0); 
+        var p4b= new Vector2d(this.pos.x1+2*this.x1axis,  this.pos.x2,                 0); 
+        var p5b= new Vector2d(this.pos.x1+2*this.x1axis,  this.pos.x2+this.x2axis,     0); 
+        var p6b= new Vector2d(this.pos.x1+2*this.x1axis,  this.pos.x2+2*this.x2axis,   0); 
+       	var p7b= new Vector2d(this.pos.x1+this.x1axis,    this.pos.x2+2*this.x2axis,   0);
+        var p8b= new Vector2d(this.pos.x1,                this.pos.x2+2*this.x2axis,   0);
 
         var ptsb= [p1b,p2b, p3b,p4b, p5b,p6b,p7b,p8b,p1b];
 
-        for (var i = 0; i <= ptsb.length - 1; i++) {
-        	ptsb[i] = ptsb[i].rotate(this.pos.x3,this.center);
+        for (var i = 0; i < ptsb.length; i++) {
+        	ptsb[i] = (ptsb[i]).rotate(this.pos.x3,this.center);
+        	
         };
-
+        // convert ptsb[i] from Vector2d to Array
         return ptsb;
 	}
 	this.shapeData.controlpoints = this.generate_circle_cpts();
@@ -132,5 +119,4 @@ Ellipse.prototype.constructor = Ellipse;
 
 //Ellipse = function... proto = phys_obj
 
-var test
 //obstacle = function no proto, no speed, mass = inifity, i.e. wall
